4daa212566f5451877190692050cc6f264acd5c1
Claudia Maciel <clc_80@yahoo.com>
2021-05-12 14:35:47 -0700

    Fixed Typo

--- Running/Model/RunningRoute.swift
+++ Running/Model/RunningRoute.swift
@@ -10,8 +10,10 @@ import MapKit
 
 class RunningRoute: NSObject, MKAnnotation {
     var coordinate: CLLocationCoordinate2D
+    var title: String?
     
     init(coordinate: CLLocationCoordinate2D) {
         self.coordinate = coordinate
+        self.title = "Start Location"
     }
 }
